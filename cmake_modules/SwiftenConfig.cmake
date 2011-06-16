cbd571766ecb8f5bc3d7db3a37b4cfecbc28747d
HanzZ <hanzz.k@gmail.com>
2011-06-17 00:07:09 +0200

    and now it works... we will see...

--- cmake_modules/SwiftenConfig.cmake
+++ cmake_modules/SwiftenConfig.cmake
@@ -2,18 +2,15 @@ FIND_LIBRARY(SWIFTEN_LIBRARY NAMES Swiften)
 FIND_PATH(SWIFTEN_INCLUDE_DIR NAMES "Swiften.h" PATH_SUFFIXES libSwiften Swiften )
 
 if( SWIFTEN_LIBRARY AND SWIFTEN_INCLUDE_DIR )
-# 	find_program(SWIFTEN_CONFIG_EXECUTABLE NAMES swiften-config DOC "swiften-config executable")
-# 	set( SWIFTEN_CFLAGS "" )
-# 	if (SWIFTEN_CONFIG_EXECUTABLE)
-# 		execute_process(
-# 			COMMAND swiften-config --libs
-# 			OUTPUT_VARIABLE SWIFTEN_LIBRARY)
-# 		execute_process(
-# 			COMMAND swiften-config --cflags
-# 			OUTPUT_VARIABLE SWIFTEN_CFLAGS)
-# 		string(REGEX REPLACE "[\r\n]"                  " " SWIFTEN_LIBRARY "${SWIFTEN_LIBRARY}")
-# 		string(REGEX REPLACE " +$"                     ""  SWIFTEN_LIBRARY "${SWIFTEN_LIBRARY}")
-# 	endif()
+	find_program(SWIFTEN_CONFIG_EXECUTABLE NAMES swiften-config DOC "swiften-config executable")
+	set( SWIFTEN_CFLAGS "" )
+	if (SWIFTEN_CONFIG_EXECUTABLE)
+		execute_process(
+			COMMAND swiften-config --libs
+			OUTPUT_VARIABLE SWIFTEN_LIBRARY)
+		string(REGEX REPLACE "[\r\n]"                  " " SWIFTEN_LIBRARY "${SWIFTEN_LIBRARY}")
+		string(REGEX REPLACE " +$"                     ""  SWIFTEN_LIBRARY "${SWIFTEN_LIBRARY}")
+	endif()
 
 	set( SWIFTEN_INCLUDE_DIR ${SWIFTEN_INCLUDE_DIR}/.. )
 	message( STATUS "Found libSwiften: ${SWIFTEN_LIBRARY}, ${SWIFTEN_INCLUDE_DIR}")
