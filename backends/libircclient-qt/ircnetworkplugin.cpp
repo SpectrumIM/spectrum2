fdc1a4199a2a1e6f9300fb520e6361b14f78aaa9
HanzZ <hanzz.k@gmail.com>
2011-10-24 22:33:15 +0200

    IRC backend almost work in gateway mode

--- backends/libircclient-qt/ircnetworkplugin.cpp
+++ backends/libircclient-qt/ircnetworkplugin.cpp
@@ -22,12 +22,18 @@ void IRCNetworkPlugin::sendData(const std::string &string) {
 }
 
 void IRCNetworkPlugin::handleLoginRequest(const std::string &user, const std::string &legacyName, const std::string &password) {
-	MyIrcSession *session = new MyIrcSession(user, this);
-	std::string h = user.substr(0, user.find("@"));
-	session->setNick(QString::fromStdString(h.substr(0, h.find("%"))));
-	session->connectToServer(QString::fromStdString(h.substr(h.find("%") + 1)), 6667);
-	std::cout << "CONNECTING IRC NETWORK " << h.substr(h.find("%") + 1) << "\n";
-	m_sessions[user] = session;
+	// Server is in server-mode, so user is JID of server when we want to connect
+	if (CONFIG_BOOL(config, "service.server_mode")) {
+		MyIrcSession *session = new MyIrcSession(user, this);
+		std::string h = user.substr(0, user.find("@"));
+		session->setNick(QString::fromStdString(h.substr(0, h.find("%"))));
+		session->connectToServer(QString::fromStdString(h.substr(h.find("%") + 1)), 6667);
+		std::cout << "CONNECTING IRC NETWORK " << h.substr(h.find("%") + 1) << "\n";
+		m_sessions[user] = session;
+	}
+	else {
+		handleConnected(user);
+	}
 }
 
 void IRCNetworkPlugin::handleLogoutRequest(const std::string &user, const std::string &legacyName) {
@@ -38,27 +44,54 @@ void IRCNetworkPlugin::handleLogoutRequest(const std::string &user, const std::s
 }
 
 void IRCNetworkPlugin::handleMessageSendRequest(const std::string &user, const std::string &legacyName, const std::string &message, const std::string &/*xhtml*/) {
-	std::cout << "MESSAGE " << user << " " << legacyName << "\n";
 	if (m_sessions[user] == NULL)
 		return;
-	m_sessions[user]->message(QString::fromStdString(legacyName), QString::fromStdString(message));
+
+	std::string r = legacyName;
+	if (!CONFIG_BOOL(config, "service.server_mode")) {
+		r = legacyName.substr(0, r.find("@"));
+	}
+	std::cout << "MESSAGE " << user << " " << r << "\n";
+	m_sessions[user]->message(QString::fromStdString(r), QString::fromStdString(message));
 	std::cout << "SENT\n";
 }
 
 void IRCNetworkPlugin::handleJoinRoomRequest(const std::string &user, const std::string &room, const std::string &nickname, const std::string &password) {
 	std::cout << "JOIN\n";
-	if (m_sessions[user] == NULL)
-		return;
-	m_sessions[user]->addAutoJoinChannel(QString::fromStdString(room));
-	m_sessions[user]->join(QString::fromStdString(room), QString::fromStdString(password));
+	std::string r = room;
+	if (m_sessions[user] == NULL) {
+		// in gateway mode we want to login this user to network according to legacyName
+		if (room.find("%") != std::string::npos) {
+			// suffix is %irc.freenode.net to let MyIrcSession return #room%irc.freenode.net
+			MyIrcSession *session = new MyIrcSession(user, this, room.substr(room.find("%")));
+			session->setNick(QString::fromStdString(nickname));
+			session->connectToServer(QString::fromStdString(room.substr(room.find("%") + 1)), 6667);
+			std::cout << "CONNECTING IRC NETWORK " << room.substr(room.find("%") + 1) << "\n";
+			std::cout << "SUFFIX " << room.substr(room.find("%")) << "\n";
+			m_sessions[user] = session;
+			r = room.substr(0, room.find("%"));
+			std::cout << "room=" << r << "\n";
+		}
+		else {
+			return;
+		}
+	}
+	m_sessions[user]->addAutoJoinChannel(QString::fromStdString(r));
+	m_sessions[user]->join(QString::fromStdString(r), QString::fromStdString(password));
 	// update nickname, because we have nickname per session, no nickname per room.
-	handleRoomNicknameChanged(user, room, m_sessions[user]->nick().toStdString());
+	handleRoomNicknameChanged(user, r, m_sessions[user]->nick().toStdString());
 }
 
 void IRCNetworkPlugin::handleLeaveRoomRequest(const std::string &user, const std::string &room) {
 	std::cout << "PART\n";
 	if (m_sessions[user] == NULL)
 		return;
-	m_sessions[user]->part(QString::fromStdString(room));
-	m_sessions[user]->removeAutoJoinChannel(QString::fromStdString(room));
+
+	std::string r = room;
+	if (!CONFIG_BOOL(config, "service.server_mode")) {
+		r = room.substr(0, room.find("%"));
+	}
+
+	m_sessions[user]->part(QString::fromStdString(r));
+	m_sessions[user]->removeAutoJoinChannel(QString::fromStdString(r));
 }
