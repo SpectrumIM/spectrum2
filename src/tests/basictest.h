848f672b07e5e951a984448d8a3587c4499d6d5a
HanzZ <hanzz.k@gmail.com>
2011-10-26 00:16:03 +0200

    Use BasicTest in tests

---
+++ src/tests/basictest.h
@@ -0,0 +1,117 @@
+/**
+ * libtransport -- C++ library for easy XMPP Transports development
+ *
+ * Copyright (C) 2011, Jan Kaluza <hanzz.k@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02111-1301  USA
+ */
+
+#pragma once
+
+#include <vector>
+#include "Swiften/Swiften.h"
+#include "Swiften/Queries/SetResponder.h"
+#include "transport/conversation.h"
+#include "transport/conversationmanager.h"
+#include "transport/userregistry.h"
+#include "transport/config.h"
+#include "transport/storagebackend.h"
+#include "transport/user.h"
+#include "transport/transport.h"
+#include "transport/conversation.h"
+#include "transport/usermanager.h"
+#include "transport/localbuddy.h"
+
+#include <Swiften/Swiften.h>
+#include <Swiften/EventLoop/DummyEventLoop.h>
+#include <Swiften/Server/Server.h>
+#include <Swiften/Network/DummyNetworkFactories.h>
+#include <Swiften/Network/DummyConnectionServer.h>
+#include "Swiften/Server/ServerStanzaChannel.h"
+#include "Swiften/Server/ServerFromClientSession.h"
+#include "Swiften/Parser/PayloadParsers/FullPayloadParserFactoryCollection.h"
+
+using namespace Transport;
+
+class TestingConversation : public Conversation {
+	public:
+		TestingConversation(ConversationManager *conversationManager, const std::string &legacyName, bool muc = false) : Conversation(conversationManager, legacyName, muc) {
+		}
+
+		// Called when there's new message to legacy network from XMPP network
+		void sendMessage(boost::shared_ptr<Swift::Message> &message) {
+
+		}
+};
+
+class TestingFactory : public Factory {
+	public:
+		TestingFactory() {
+		}
+
+		// Creates new conversation (NetworkConversation in this case)
+		Conversation *createConversation(ConversationManager *conversationManager, const std::string &legacyName) {
+			Conversation *nc = new TestingConversation(conversationManager, legacyName);
+			return nc;
+		}
+
+		// Creates new LocalBuddy
+		Buddy *createBuddy(RosterManager *rosterManager, const BuddyInfo &buddyInfo) {
+			LocalBuddy *buddy = new LocalBuddy(rosterManager, buddyInfo.id);
+			buddy->setAlias(buddyInfo.alias);
+			buddy->setName(buddyInfo.legacyName);
+			buddy->setSubscription(buddyInfo.subscription);
+			buddy->setGroups(buddyInfo.groups);
+			buddy->setFlags((BuddyFlag) buddyInfo.flags);
+			if (buddyInfo.settings.find("icon_hash") != buddyInfo.settings.end())
+				buddy->setIconHash(buddyInfo.settings.find("icon_hash")->second.s);
+			return buddy;
+		}
+};
+
+class BasicTest : public Swift::XMPPParserClient {
+
+	public:
+		void setMeUp (void);
+
+		void tearMeDown (void);
+
+	void handleDataReceived(const Swift::SafeByteArray &data);
+
+	void handleStreamStart(const Swift::ProtocolHeader&);
+
+	void handleElement(boost::shared_ptr<Swift::Element> element);
+
+	void handleStreamEnd();
+
+	Swift::Stanza *getStanza(boost::shared_ptr<Swift::Element> element);
+
+	protected:
+		bool streamEnded;
+		UserManager *userManager;
+		boost::shared_ptr<Swift::ServerFromClientSession> serverFromClientSession;
+		Swift::FullPayloadSerializerCollection* payloadSerializers;
+		Swift::FullPayloadParserFactoryCollection* payloadParserFactories;
+		Swift::XMPPParser *parser;
+		UserRegistry *userRegistry;
+		Config *cfg;
+		Swift::Server *server;
+		Swift::DummyNetworkFactories *factories;
+		Swift::DummyEventLoop *loop;
+		TestingFactory *factory;
+		Component *component;
+		std::vector<boost::shared_ptr<Swift::Element> > received;
+};
+
--- src/tests/usermanager.cpp
+++ src/tests/usermanager.cpp
@@ -16,46 +16,11 @@
 #include "Swiften/Server/ServerStanzaChannel.h"
 #include "Swiften/Server/ServerFromClientSession.h"
 #include "Swiften/Parser/PayloadParsers/FullPayloadParserFactoryCollection.h"
+#include "basictest.h"
 
 using namespace Transport;
 
-class TestingConversation : public Conversation {
-	public:
-		TestingConversation(ConversationManager *conversationManager, const std::string &legacyName, bool muc = false) : Conversation(conversationManager, legacyName, muc) {
-		}
-
-		// Called when there's new message to legacy network from XMPP network
-		void sendMessage(boost::shared_ptr<Swift::Message> &message) {
-			
-		}
-};
-
-class TestingFactory : public Factory {
-	public:
-		TestingFactory() {
-		}
-
-		// Creates new conversation (NetworkConversation in this case)
-		Conversation *createConversation(ConversationManager *conversationManager, const std::string &legacyName) {
-			Conversation *nc = new TestingConversation(conversationManager, legacyName);
-			return nc;
-		}
-
-		// Creates new LocalBuddy
-		Buddy *createBuddy(RosterManager *rosterManager, const BuddyInfo &buddyInfo) {
-			LocalBuddy *buddy = new LocalBuddy(rosterManager, buddyInfo.id);
-			buddy->setAlias(buddyInfo.alias);
-			buddy->setName(buddyInfo.legacyName);
-			buddy->setSubscription(buddyInfo.subscription);
-			buddy->setGroups(buddyInfo.groups);
-			buddy->setFlags((BuddyFlag) buddyInfo.flags);
-			if (buddyInfo.settings.find("icon_hash") != buddyInfo.settings.end())
-				buddy->setIconHash(buddyInfo.settings.find("icon_hash")->second.s);
-			return buddy;
-		}
-};
-
-class UserManagerTest : public CPPUNIT_NS :: TestFixture, public Swift::XMPPParserClient {
+class UserManagerTest : public CPPUNIT_NS :: TestFixture, public BasicTest {
 	CPPUNIT_TEST_SUITE(UserManagerTest);
 	CPPUNIT_TEST(connectUser);
 	CPPUNIT_TEST(handleProbePresence);
@@ -64,66 +29,12 @@ class UserManagerTest : public CPPUNIT_NS :: TestFixture, public Swift::XMPPPars
 
 	public:
 		void setUp (void) {
-			streamEnded = false;
-			std::istringstream ifs("service.server_mode = 1\n");
-			cfg = new Config();
-			cfg->load(ifs);
-
-			factory = new TestingFactory();
-
-			loop = new Swift::DummyEventLoop();
-			factories = new Swift::DummyNetworkFactories(loop);
-
-			userRegistry = new UserRegistry(cfg, factories);
-
-			component = new Component(loop, factories, cfg, factory, userRegistry);
-			component->start();
-
-			userManager = new UserManager(component, userRegistry);
-
-			payloadSerializers = new Swift::FullPayloadSerializerCollection();
-			payloadParserFactories = new Swift::FullPayloadParserFactoryCollection();
-			parser = new Swift::XMPPParser(this, payloadParserFactories, factories->getXMLParserFactory());
-
-			serverFromClientSession = boost::shared_ptr<Swift::ServerFromClientSession>(new Swift::ServerFromClientSession("id", factories->getConnectionFactory()->createConnection(), 
-					payloadParserFactories, payloadSerializers, userRegistry, factories->getXMLParserFactory(), Swift::JID("user@localhost/resource")));
-			serverFromClientSession->startSession();
-
-			serverFromClientSession->onDataWritten.connect(boost::bind(&UserManagerTest::handleDataReceived, this, _1));
-
-			dynamic_cast<Swift::ServerStanzaChannel *>(component->getStanzaChannel())->addSession(serverFromClientSession);
-			parser->parse("<stream:stream xmlns='jabber:client' xmlns:stream='http://etherx.jabber.org/streams' to='localhost' version='1.0'>");
-			received.clear();
-			loop->processEvents();
+			setMeUp();
 		}
 
 		void tearDown (void) {
-			dynamic_cast<Swift::ServerStanzaChannel *>(component->getStanzaChannel())->removeSession(serverFromClientSession);
-			delete component;
-			delete userRegistry;
-			delete factories;
-			delete factory;
-			delete loop;
-			delete cfg;
-			delete parser;
-			received.clear();
+			tearMeDown();
 		}
-
-	void handleDataReceived(const Swift::SafeByteArray &data) {
-		parser->parse(safeByteArrayToString(data));
-	}
-
-	void handleStreamStart(const Swift::ProtocolHeader&) {
-		
-	}
-
-	void handleElement(boost::shared_ptr<Swift::Element> element) {
-		received.push_back(element);
-	}
-
-	void handleStreamEnd() {
-		streamEnded = true;
-	}
 
 	void connectUser() {
 		CPPUNIT_ASSERT_EQUAL(0, userManager->getUserCount());
@@ -175,27 +86,6 @@ class UserManagerTest : public CPPUNIT_NS :: TestFixture, public Swift::XMPPPars
 		CPPUNIT_ASSERT_EQUAL(Swift::Presence::Unavailable, presence->getType());
 	}
 
-	Swift::Stanza *getStanza(boost::shared_ptr<Swift::Element> element) {
-		Swift::Stanza *stanza = dynamic_cast<Swift::Stanza *>(element.get());
-		CPPUNIT_ASSERT(stanza);
-		return stanza;
-	}
-
-	private:
-		bool streamEnded;
-		UserManager *userManager;
-		boost::shared_ptr<Swift::ServerFromClientSession> serverFromClientSession;
-		Swift::FullPayloadSerializerCollection* payloadSerializers;
-		Swift::FullPayloadParserFactoryCollection* payloadParserFactories;
-		Swift::XMPPParser *parser;
-		UserRegistry *userRegistry;
-		Config *cfg;
-		Swift::Server *server;
-		Swift::DummyNetworkFactories *factories;
-		Swift::DummyEventLoop *loop;
-		TestingFactory *factory;
-		Component *component;
-		std::vector<boost::shared_ptr<Swift::Element> > received;
 };
 
 CPPUNIT_TEST_SUITE_REGISTRATION (UserManagerTest);
