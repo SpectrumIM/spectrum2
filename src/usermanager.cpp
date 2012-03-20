7067865738d06ea9b649caf06e6c729893fe405d
HanzZ <hanzz.k@gmail.com>
2012-03-20 22:06:51 +0100

    Send admin contac tot user

--- src/usermanager.cpp
+++ src/usermanager.cpp
@@ -152,6 +152,21 @@ void UserManager::handlePresence(Swift::Presence::ref presence) {
 	if (!user) {
 		// Admin user is not legacy network user, so do not create User class instance for him
 		if (m_component->inServerMode() && CONFIG_STRING(m_component->getConfig(), "service.admin_jid") == presence->getFrom().toBare().toString()) {
+			// Send admin contact to the user.
+			Swift::RosterPayload::ref payload = Swift::RosterPayload::ref(new Swift::RosterPayload());
+			Swift::RosterItemPayload item;
+			item.setJID(m_component->getJID());
+			item.setName("Admin");
+			item.setSubscription(Swift::RosterItemPayload::Both);
+			payload->addItem(item);
+
+			Swift::SetRosterRequest::ref request = Swift::SetRosterRequest::create(payload, presence->getFrom(), m_component->getIQRouter());
+			request->send();
+
+			Swift::Presence::ref response = Swift::Presence::create();
+			response->setTo(presence->getFrom());
+			response->setFrom(m_component->getJID());
+			m_component->getStanzaChannel()->sendPresence(response);
 			return;
 		}
 
