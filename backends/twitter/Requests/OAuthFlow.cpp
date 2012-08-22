b416fdd826b8f75c7bc415d6c7922f49a18c84ed
Jan Kaluza <hanzz.k@gmail.com>
2012-08-22 09:48:25 +0200

    Send PIN request from twitter.com not twitter-account

--- backends/twitter/Requests/OAuthFlow.cpp
+++ backends/twitter/Requests/OAuthFlow.cpp
@@ -9,11 +9,11 @@ void OAuthFlow::finalize()
 {
 	if (!success) {
 		LOG4CXX_ERROR(logger, "Error creating twitter authorization url!");
-		np->handleMessage(user, "twitter-account", "Error creating twitter authorization url!");
+		np->handleMessage(user, "twitter.com", "Error creating twitter authorization url!");
 		np->handleLogoutRequest(user, username);
 	} else {
-		np->handleMessage(user, "twitter-account", std::string("Please visit the following link and authorize this application: ") + authUrl);
-		np->handleMessage(user, "twitter-account", std::string("Please reply with the PIN provided by twitter. Prefix the pin with '#pin'. Ex. '#pin 1234'"));
+		np->handleMessage(user, "twitter.com", std::string("Please visit the following link and authorize this application: ") + authUrl);
+		np->handleMessage(user, "twitter.com", std::string("Please reply with the PIN provided by twitter. Prefix the pin with '#pin'. Ex. '#pin 1234'"));
 		np->OAuthFlowComplete(user, twitObj);
 	}	
 }
