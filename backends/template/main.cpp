8b60685886212d7ee29482abdefb00c6fb99d9db
HanzZ <hanzz.k@gmail.com>
2012-09-12 10:18:51 +0200

    Split Template backend into more files

--- backends/template/main.cpp
+++ backends/template/main.cpp
@@ -1,3 +1,5 @@
+#include "plugin.h"
+
 // Transport includes
 #include "transport/config.h"
 #include "transport/networkplugin.h"
@@ -18,71 +20,6 @@
 using namespace boost::filesystem;
 using namespace boost::program_options;
 using namespace Transport;
-
-DEFINE_LOGGER(logger, "Backend Template");
-
-// eventloop
-Swift::SimpleEventLoop *loop_;
-
-// Plugin
-class TemplatePlugin;
-TemplatePlugin * np = NULL;
-
-class TemplatePlugin : public NetworkPlugin {
-	public:
-		Swift::BoostNetworkFactories *m_factories;
-		Swift::BoostIOServiceThread m_boostIOServiceThread;
-		boost::shared_ptr<Swift::Connection> m_conn;
-
-		TemplatePlugin(Config *config, Swift::SimpleEventLoop *loop, const std::string &host, int port) : NetworkPlugin() {
-			this->config = config;
-			m_factories = new Swift::BoostNetworkFactories(loop);
-			m_conn = m_factories->getConnectionFactory()->createConnection();
-			m_conn->onDataRead.connect(boost::bind(&TemplatePlugin::_handleDataRead, this, _1));
-			m_conn->connect(Swift::HostAddressPort(Swift::HostAddress(host), port));
-
-			LOG4CXX_INFO(logger, "Starting the plugin.");
-		}
-
-		// NetworkPlugin uses this method to send the data to networkplugin server
-		void sendData(const std::string &string) {
-			m_conn->write(Swift::createSafeByteArray(string));
-		}
-
-		// This method has to call handleDataRead with all received data from network plugin server
-		void _handleDataRead(boost::shared_ptr<Swift::SafeByteArray> data) {
-			std::string d(data->begin(), data->end());
-			handleDataRead(d);
-		}
-
-		void handleLoginRequest(const std::string &user, const std::string &legacyName, const std::string &password) {
-			handleConnected(user);
-			LOG4CXX_INFO(logger, user << ": Added buddy - Echo.");
-			handleBuddyChanged(user, "echo", "Echo", std::vector<std::string>(), pbnetwork::STATUS_ONLINE);
-		}
-
-		void handleLogoutRequest(const std::string &user, const std::string &legacyName) {
-		}
-
-		void handleMessageSendRequest(const std::string &user, const std::string &legacyName, const std::string &message, const std::string &xhtml = "") {
-			LOG4CXX_INFO(logger, "Sending message from " << user << " to " << legacyName << ".");
-			if (legacyName == "echo") {
-				handleMessage(user, legacyName, message);
-			}
-		}
-
-		void handleBuddyUpdatedRequest(const std::string &user, const std::string &buddyName, const std::string &alias, const std::vector<std::string> &groups) {
-			LOG4CXX_INFO(logger, user << ": Added buddy " << buddyName << ".");
-			handleBuddyChanged(user, buddyName, alias, groups, pbnetwork::STATUS_ONLINE);
-		}
-
-		void handleBuddyRemovedRequest(const std::string &user, const std::string &buddyName, const std::vector<std::string> &groups) {
-
-		}
-
-	private:
-		Config *config;
-};
 
 #ifndef _WIN32
 
@@ -124,9 +61,8 @@ int main (int argc, char* argv[]) {
 	Logging::initBackendLogging(cfg);
 
 	Swift::SimpleEventLoop eventLoop;
-	loop_ = &eventLoop;
-	np = new TemplatePlugin(cfg, &eventLoop, host, port);
-	loop_->run();
+	Plugin p(cfg, &eventLoop, host, port);
+	eventLoop.run();
 
 	return 0;
 }
---
+++ backends/template/plugin.cpp
@@ -0,0 +1,62 @@
+#include "plugin.h"
+// Transport includes
+#include "transport/config.h"
+#include "transport/networkplugin.h"
+#include "transport/logging.h"
+
+// Swiften
+#include "Swiften/Swiften.h"
+
+// Boost
+#include <boost/algorithm/string.hpp>
+using namespace boost::filesystem;
+using namespace boost::program_options;
+using namespace Transport;
+
+DEFINE_LOGGER(logger, "Backend Template");
+
+Plugin::Plugin(Config *config, Swift::SimpleEventLoop *loop, const std::string &host, int port) : NetworkPlugin() {
+	this->config = config;
+	m_factories = new Swift::BoostNetworkFactories(loop);
+	m_conn = m_factories->getConnectionFactory()->createConnection();
+	m_conn->onDataRead.connect(boost::bind(&Plugin::_handleDataRead, this, _1));
+	m_conn->connect(Swift::HostAddressPort(Swift::HostAddress(host), port));
+
+	LOG4CXX_INFO(logger, "Starting the plugin.");
+}
+
+// NetworkPlugin uses this method to send the data to networkplugin server
+void Plugin::sendData(const std::string &string) {
+	m_conn->write(Swift::createSafeByteArray(string));
+}
+
+// This method has to call handleDataRead with all received data from network plugin server
+void Plugin::_handleDataRead(boost::shared_ptr<Swift::SafeByteArray> data) {
+	std::string d(data->begin(), data->end());
+	handleDataRead(d);
+}
+
+void Plugin::handleLoginRequest(const std::string &user, const std::string &legacyName, const std::string &password) {
+	handleConnected(user);
+	LOG4CXX_INFO(logger, user << ": Added buddy - Echo.");
+	handleBuddyChanged(user, "echo", "Echo", std::vector<std::string>(), pbnetwork::STATUS_ONLINE);
+}
+
+void Plugin::handleLogoutRequest(const std::string &user, const std::string &legacyName) {
+}
+
+void Plugin::handleMessageSendRequest(const std::string &user, const std::string &legacyName, const std::string &message, const std::string &xhtml) {
+	LOG4CXX_INFO(logger, "Sending message from " << user << " to " << legacyName << ".");
+	if (legacyName == "echo") {
+		handleMessage(user, legacyName, message);
+	}
+}
+
+void Plugin::handleBuddyUpdatedRequest(const std::string &user, const std::string &buddyName, const std::string &alias, const std::vector<std::string> &groups) {
+	LOG4CXX_INFO(logger, user << ": Added buddy " << buddyName << ".");
+	handleBuddyChanged(user, buddyName, alias, groups, pbnetwork::STATUS_ONLINE);
+}
+
+void Plugin::handleBuddyRemovedRequest(const std::string &user, const std::string &buddyName, const std::vector<std::string> &groups) {
+
+}
