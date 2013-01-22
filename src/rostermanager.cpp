799350e23fbf09f2dd15b1db378e7b898b0d88e8
Darchigh <Darchigh@gmail.com>
2013-01-22 10:29:36 +0100

    Small change...

--- src/rostermanager.cpp
+++ src/rostermanager.cpp
@@ -282,14 +282,12 @@ void RosterManager::handleRemoteRosterResponse(boost::shared_ptr<Swift::RosterPa
 	m_supportRemoteRoster = true;
 
 	//If we receive empty RosterPayload on login (not register) initiate full RosterPush
-	if(!m_buddies.empty()){
-		if(payload->getItems().empty()){
+	if(!m_buddies.empty() && payload->getItems().empty()){
 			LOG4CXX_INFO(logger, "Received empty Roster upon login. Pushing full Roster.");
 			for(std::map<std::string, Buddy *, std::less<std::string>, boost::pool_allocator< std::pair<std::string, Buddy *> > >::const_iterator c_it = m_buddies.begin();
 					c_it != m_buddies.end(); c_it++) {
 				sendBuddyRosterPush(c_it->second);
 			}
-		}
 	}
 	return;
 
