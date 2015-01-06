54ae28e4838a75ef1e8f440ef34033fd01370d5f
Vitaly Takmazov <vitalyster@gmail.com>
2015-01-07 01:50:13 +0300

    add build_spectrum2.sh for debian/ubuntu

---
+++ packaging/debian/build_spectrum2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -x
+
+vercount=$(git rev-list --all | wc -l)
+gitrev=$(git rev-parse --short HEAD | sed 's/\(^[0-9\.]*\)-/\1~/')
+version=$vercount-$gitrev
+
+sed -i "1s/\((.*)\)/(1:$version-1)/" debian/changelog
+
+rm -rf spectrum2_*
+
+git clone ../../.git spectrum2-$version
+
+tar -czf spectrum2_$version.orig.tar.gz spectrum2-$version
+
+cp -r debian spectrum2-$version/debian
+
+cd spectrum2-$version
+debuild -i -us -uc -S
+
+cd ..
+
+rm -rf spectrum2-$version
