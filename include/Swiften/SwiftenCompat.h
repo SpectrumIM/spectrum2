0f84876118718138ed1a6727e98bf168521d0d08
Steffen Vogel <post@steffenvogel.de>
2017-10-30 16:55:36 +0100

    fix typo in compatability macro

--- include/Swiften/SwiftenCompat.h
+++ include/Swiften/SwiftenCompat.h
@@ -1,18 +1,18 @@
 /*
  * Swift compatibility
- * 
+ *
  * Copyright (c) 2016, Vladimir Matena <vlada.matena@gmail.com>
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -36,12 +36,12 @@
  */
 
 #if (SWIFTEN_VERSION >= 0x040000)
-#define SWIFTEN_UNQPTR std::unique_ptr
+#define SWIFTEN_UNIQUE_PTR std::unique_ptr
 #define SWIFTEN_SHRPTR_NAMESPACE std
 #define SWIFTEN_SIGNAL_NAMESPACE boost::signals2
 #define SWIFT_HOSTADDRESS(x) *(Swift::HostAddress::fromString(x))
 #else
-#define SWIFTEN_UNQPTR std::auto_ptr
+#define SWIFTEN_UNIQUE_PTR std::auto_ptr
 #define SWIFTEN_SHRPTR_NAMESPACE boost
 #define SWIFTEN_SIGNAL_NAMESPACE boost::signals
 #define SWIFT_HOSTADDRESS(x) Swift::HostAddress(x)
