fb2efa080050579aa01859ef01a14478424977c9
Vitaly Takmazov <vitalyster@gmail.com>
2019-07-15 11:18:06 +0300

    Revert "Some binary tests for backend_libpurple's loopback tracker."
    This reverts commit cde21e11501900078cbcf64afb9beb88328a8c52.

--- packaging/debian/build_spectrum2.sh
+++ packaging/debian/build_spectrum2.sh
@@ -9,9 +9,9 @@ hash debuild 2>/dev/null || ( echo >&2 "apt-get install  devscripts"; exit 1; )
 
 vercount=$(git rev-list --all | wc -l)
 gitrev=$(git rev-parse --short HEAD | sed 's/\(^[0-9\.]*\)-/\1~/')
-version=$vercount-$gitrev
+version=2.0.11
 
-sed -i "1s/\((.*)\)/(1:$version-1)/" debian/changelog
+sed -i "1s/\((.*)\)/(1:$version-2)/" debian/changelog
 
 rm -rf spectrum2_*
 
@@ -22,7 +22,7 @@ tar -czf spectrum2_$version.orig.tar.gz spectrum2-$version
 cp -r debian spectrum2-$version/debian
 
 cd spectrum2-$version
-DEB_BUILD_OPTIONS=nocheck debuild -i -us -uc
+DEB_BUILD_OPTIONS=nocheck debuild -i -us -uc -rsudo
 
 cd ..
 
