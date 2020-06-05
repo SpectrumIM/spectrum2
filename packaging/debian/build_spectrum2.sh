d5c17325d67ed0371de739687e82aaccf0473d08
Vitaly Takmazov <vitalyster@gmail.com>
2020-06-05 19:37:30 +0300

    packaging: revert wrongly commited script changes

--- packaging/debian/build_spectrum2.sh
+++ packaging/debian/build_spectrum2.sh
@@ -9,9 +9,9 @@ hash debuild 2>/dev/null || ( echo >&2 "apt-get install  devscripts"; exit 1; )
 
 vercount=$(git rev-list --all | wc -l)
 gitrev=$(git rev-parse --short HEAD | sed 's/\(^[0-9\.]*\)-/\1~/')
-version=2.0.11
+version=$vercount-$gitrev
 
-sed -i "1s/\((.*)\)/(1:$version-2)/" debian/changelog
+sed -i "1s/\((.*)\)/(1:$version-1)/" debian/changelog
 
 rm -rf spectrum2_*
 
@@ -22,7 +22,7 @@ tar -czf spectrum2_$version.orig.tar.gz spectrum2-$version
 cp -r debian spectrum2-$version/debian
 
 cd spectrum2-$version
-DEB_BUILD_OPTIONS=nocheck debuild -i -us -uc -rsudo
+DEB_BUILD_OPTIONS=nocheck debuild -i -us -uc
 
 cd ..
 
