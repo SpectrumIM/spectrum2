df84ed851d582e8fff8dc95604aa747a045b15c8
Vitaly Takmazov <vitalyster@gmail.com>
2020-07-09 10:27:33 +0300

    Docker image entrypoint improvements
    * start spectrum2_manager server
    * exit on errors

--- packaging/docker/run.sh
+++ packaging/docker/run.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-while :
-do
-	echo "Trying to start Spectrum 2 instances."
-	echo "You should mount the directory with configuration files to /etc/spectrum2/transports/."
-	echo "Check the http://spectrum.im/documentation for more information."
-	spectrum2_manager start
-	tail -f /var/log/spectrum2/*/* 2>/dev/null
-	sleep 2
-done
+echo "Trying to start Spectrum 2 instances."
+echo "You should mount the directory with configuration files to /etc/spectrum2/transports/."
+echo "Check the http://spectrum.im/documentation for more information."
+spectrum2_manager start
+spectrum2_manager server &
+tail -f /var/log/spectrum2/*/* 2>/dev/null
