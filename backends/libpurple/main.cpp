5ef365b2552153b0bf142cfd5a679c5154f0aaf1
Vitaly Takmazov <vitalyster@gmail.com>
2022-01-30 18:54:08 +0300

    libpurple: do not crash on filetransfers with empty filename

--- backends/libpurple/main.cpp
+++ backends/libpurple/main.cpp
@@ -1982,7 +1982,9 @@ static void XferCreated(PurpleXfer *xfer) {
 
  	PurpleAccount *account = purple_xfer_get_account_wrapped(xfer);
 	if (np->m_accounts.find(account) != np->m_accounts.end()) {
-		np->handleFTStart(np->m_accounts[account], xfer->who ? xfer->who : "", purple_xfer_get_filename_wrapped(xfer), purple_xfer_get_size_wrapped(xfer));
+		const char * filenameData = purple_xfer_get_filename_wrapped(xfer);
+		std::string filename(filenameData ? filenameData : "");
+		np->handleFTStart(np->m_accounts[account], xfer->who ? xfer->who : "", filename, purple_xfer_get_size_wrapped(xfer));
 	}
 }
 
