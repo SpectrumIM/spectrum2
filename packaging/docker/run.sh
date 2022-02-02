1648d05c08459ec45391885985324f1c15a92dda
Vitaly Takmazov <vitalyster@gmail.com>
2022-02-02 11:12:42 +0000

    Docker: log to stdout
    * Do not show log files from entrypoint script
    * Example logger configurations uses stdout

--- packaging/docker/run.sh
+++ packaging/docker/run.sh
@@ -15,4 +15,3 @@ echo "You should mount the directory with configuration files to /etc/spectrum2/
 echo "Check the http://spectrum.im/documentation for more information."
 spectrum2_manager start
 spectrum2_manager server &
-tail -f /var/log/spectrum2/*/* 2>/dev/null
