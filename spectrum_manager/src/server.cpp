f0b949c0b3dbd438cbbf097f83feee1bd21d8e68
Tobias Frost <tobi@debian.org>
2022-11-29 16:59:23 +0100

    Fix identation.

--- spectrum_manager/src/server.cpp
+++ spectrum_manager/src/server.cpp
@@ -71,8 +71,8 @@ Server::Server(ManagerConfig *config, const std::string &config_file) {
 	mg_mgr_init(&m_mgr, this);
 
 	if(m_password == "" || m_password == "test") {
-        std::cerr << "Rejecting empty or default admin password." << std::endl;
-        exit(1);
+		std::cerr << "Rejecting empty or default admin password." << std::endl;
+		exit(1);
 	}
 
 	struct mg_bind_opts opts;
