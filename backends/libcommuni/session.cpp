147fbbc59175ff2e042f74f1ff5502964125f861
dekeonus <dek.devel@baisenvar.info>
2026-07-27 19:05:20 +1000

    Enable building libcommuni with QT6 (#545)

--- backends/libcommuni/session.cpp
+++ backends/libcommuni/session.cpp
@@ -25,7 +25,6 @@
 #include <IrcMessage>
 #include <IrcUser>
 #include <IrcChannel>
-#include "backports.h"
 
 #include "ircnetworkplugin.h"
 
@@ -409,7 +408,7 @@ void MyIrcSession::sendMessageToFrontend(const std::string &channel, const std::
 void MyIrcSession::on_messageReceived(IrcMessage *message) {
 	IrcPrivateMessage *m = (IrcPrivateMessage *) message;
 	if (m->isRequest()) {
-		QString request = m->content().split(" ", QString::SkipEmptyParts).value(0).toUpper();
+		QString request = m->content().split(" ", Qt::SkipEmptyParts).value(0).toUpper();
 		if (request == "PING" || request == "TIME" || request == "VERSION") {
 			LOG4CXX_INFO(connectionLogger, m_user << ": " << TO_UTF8(request) << " received and has been answered");
 			return;
